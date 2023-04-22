f4754d96c3145c6f03235b0404022aada7550b5c
KOUSHKI89 <koushkitiwari7@gmail.com>
2023-04-22 21:22:47 +0530

    app theme

---
+++ lib/app_theme/constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+const Color kButtonColors = Color(0xFF279646);
+const Color kOnProfitColor = Color(0xFF5ECCA3);
+const Color kOnLossColor = Color(0xFFFF7070);
+const Color kBottomAppBarColor = Color(0xFF191826);
+const Color kHeadLineColor = Color(0xFFF2F2F2);
+const Color kSubInfoColor = Color(0xFFBFBFBF);
+
+const Color kUnselectedColor = Color(0xFF787878);
